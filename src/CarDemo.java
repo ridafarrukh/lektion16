b980b2f73054862e807b8fbb39ad8b76999cdcab
ridafarrukh <ridafarrukh92@gmail.com>
2020-10-26 12:54:54 +0100

    create file CarDemo

--- src/CarDemo.java
+++ src/CarDemo.java
@@ -5,6 +5,13 @@ public class CarDemo {
         car1 = new Car();
         System.out.println(car1);
 
+        Car car2;
+        car2 = new Car();
+        System.out.println(car2);
+
+        Car car3;
+        car3 = new Car();
+        System.out.println(car3);
 
     }
 
